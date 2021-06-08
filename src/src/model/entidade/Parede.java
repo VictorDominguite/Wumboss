2552ea9d710658ec5e15f5ded6e27f1eeaade1f9
Victor <vdominguite@gmail.com>
2021-06-08 17:34:52 -0300

    Add classes PocoVenenoso e Parede

---
+++ src/src/model/entidade/Parede.java
@@ -0,0 +1,5 @@
+package src.model.entidade;
+
+public class Parede {
+    
+}
---
+++ src/src/model/entidade/PocoVenenoso.java
@@ -0,0 +1,14 @@
+package src.model.entidade;
+
+public class PocoVenenoso {
+    private static final int DANO = 1;
+    private static final int DURACAO_EFEITO = 2;
+
+    public int getDano() {
+        return DANO;
+    }
+
+    public int getDuracaoEfeito() {
+        return DURACAO_EFEITO;
+    }
+}
