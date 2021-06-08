33b138aa9511acf5d57ada15eb5f56abaf127fb3
Thiago <thiagodonato300@gmail.com>
2021-06-08 19:29:11 -0300

    add: template do gamemodel, com alguma documentacao

--- src/src/AppWumboss.java
+++ src/src/AppWumboss.java
@@ -1,12 +1,20 @@
 package src;
 
-import src.view.*;
+import src.model.GameModel;
+import src.model.IGameModel;
+import src.view.MainView;
 
 public class AppWumboss {
 	public static void main(String[] args) {
+		//Start controller
+
+		
+		//Start model
+		IGameModel gm = new GameModel();
+		gm.montarCaverna();
+		
 		//Start view
 		MainView mv = new MainView();
-		//Start model
-		//Start controller
+		
 	}
 }
---
+++ src/src/model/GameModel.java
@@ -0,0 +1,44 @@
+package src.model;
+
+import src.model.entidade.dinamica.Heroi;
+
+public class GameModel implements IGameModel{
+	private Caverna cave;
+	private Heroi hero;
+	private Inventario inv;
+	
+	public void montarCaverna() {
+		
+	}
+
+	/* Retorna o estado da caverna relevante ao View
+	 * Isso inclui a sala em que o herói está, e
+	 * possivelmente as salas vizinhas. */
+	public String[][] getCaveState() {
+		if(cave == null)
+			System.err.println("A caverna não foi criada ainda!");
+		
+		return null;
+	}
+
+	/* Retorna o estado do herói relevante ao View
+	 * Isso inclui a vida atual dele, seu poder de
+	 * ataque e defesa, possivelmente seu nome, etc */
+	public String[][] getHeroState() {
+		if(hero == null)
+			System.err.println("O herói não existe ainda!");
+		
+		return null;
+	}
+
+	/* Retorna o inventario do heroi relevante ao View
+	 * Isso inclui os itens coletados, seus nomes, 
+	 * descricoes, se estao equipados, etc */
+	public String[][] getInventoryState() {
+		if(inv == null)
+			System.err.println("O inventário não existe ainda!");
+		
+		return null;
+	}
+
+}
