1320bb5a7b3e60962d0ffa2f5e43839984864240
Thiago <thiagodonato300@gmail.com>
2021-06-08 15:46:33 -0300

    fix: centraliza labels

--- src/src/view/panels/InventoryPanel.java
+++ src/src/view/panels/InventoryPanel.java
@@ -1,16 +1,26 @@
 package src.view.panels;
 import javax.swing.*;
+
+import src.model.entidade.itens.Item;
+
 import java.awt.GridLayout;
 
 public class InventoryPanel extends Panel{
-    public InventoryPanel(){
+	private static final long serialVersionUID = 5112485242913536772L;
+
+	public InventoryPanel(){
         super(new GridLayout(2, 5));
         
         setWeightX(10);
         setWeightY(3);
 
         for(int i = 0; i < 10; i++){
-            add(new JLabel("a"));
+            add(new JLabel("" + i, JLabel.CENTER));
         }
+    }
+    
+    private Item[] getItems() {
+    	//requer pela interface view-model os itens do inventario
+    	return null;
     }
 }
